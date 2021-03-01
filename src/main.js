0a2a87ae407c092b58018a8e9e6f8a2a0fee917e
jgmw9625 <cc620026@gmail.com>
2021-03-01 20:42:34 +0800

    save

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import elementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import index from './index.vue'
 import router from './route/index'
 import store from './store/index'
 
 Vue.config.productionTip = false
+Vue.config.devtools = true
 Vue.use(elementUI)
 
 new Vue({
